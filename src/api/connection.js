060f3bcaaecc7b40541e8b68ea40b92f7d5fb30d
alvaropms <alvaromelquiades2001@gmail.com>
2022-02-07 15:41:10 -0300

    teste

--- src/api/connection.js
+++ src/api/connection.js
@@ -5,6 +5,15 @@ export const weather =  axios.create({
     headers:{
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+        
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        
+        'accept-language': 'pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+        'cache-control': 'max-age=0',
+        
+        'if-none-match': 'W/"3267ea5d99e709c96ada25c0b213303b"',
+
+        'upgrade-insecure-requests': 1
     }
 })
