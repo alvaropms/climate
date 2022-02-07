27544adb5caa0f54429060cac2b639c3cbc5370e
alvaropms <alvaromelquiades2001@gmail.com>
2022-02-07 15:17:47 -0300

    header api

--- src/api/connection.js
+++ src/api/connection.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const weather =  axios.create({
-    baseURL:'https://api.hgbrasil.com/weather?key=253f4c5f&user_ip=remote'
+    baseURL:'https://api.hgbrasil.com/weather?key=253f4c5f&user_ip=remote',
+    headers:{'Access-Control-Allow-Origin': '*'}
 })
