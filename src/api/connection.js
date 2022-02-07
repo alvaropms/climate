7825769fc962cac8394558416fbe66ce6b55d96d
alvaropms <alvaromelquiades2001@gmail.com>
2022-02-07 15:33:34 -0300

    alteração de api

--- src/api/connection.js
+++ src/api/connection.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const weather =  axios.create({
-    baseURL:'https://api.hgbrasil.com/weather?key=253f4c5f&user_ip=remote',
-    headers:{'Access-Control-Allow-Origin': '*'}
+    baseURL:'https://api.hgbrasil.com/weather?key=aa0eca0d&user_ip=remote',
+    headers:{
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+    }
 })
