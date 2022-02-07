7e3d089d3b32ed3cfcdc64575b9d0786a77aab42
alvaropms <alvaromelquiades2001@gmail.com>
2022-02-07 15:14:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { weather } from "./api/connection";
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    weather.get('').then(
+      resp => setMessage(resp.data)
+    );
+  }, []);
+  return (
+    <>
+    <p>
+      {message.temp}
+    </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/api/connection.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const weather =  axios.create({
+    baseURL:'https://api.hgbrasil.com/weather?key=253f4c5f&user_ip=remote'
+})
